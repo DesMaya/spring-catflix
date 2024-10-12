8aabb61a059ea7b8a1aab21322c6f05d420593a6
Maya <mayaflorentinodasilva@gmail.com>
2024-10-12 18:33:54 -0300

    Arrumando o record para ignorar os dados não mapeados da api

--- src/main/java/estudos/maya/model/DadosSerie.java
+++ src/main/java/estudos/maya/model/DadosSerie.java
@@ -1,7 +1,9 @@
 package estudos.maya.model;
 
 import com.fasterxml.jackson.annotation.JsonAlias;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
+@JsonIgnoreProperties(ignoreUnknown = true)
 public record DadosSerie(
         @JsonAlias("Title") String titulo,
         @JsonAlias("totalSeasons") Integer totalTemporadas,
