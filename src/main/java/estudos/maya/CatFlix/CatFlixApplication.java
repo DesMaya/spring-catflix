dd55fbbcb68927ab7184e17fa9e3076b55206fc3
Maya <mayaflorentinodasilva@gmail.com>
2024-10-12 16:57:14 -0300

    Formatando espaços para melhor legibilidades e removendo redundância

--- src/main/java/estudos/maya/CatFlix/CatFlixApplication.java
+++ src/main/java/estudos/maya/CatFlix/CatFlixApplication.java
@@ -1,9 +1,10 @@
 package estudos.maya.CatFlix;
 
+import estudos.maya.CatFlix.service.ConsumoApi;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+//https://www.omdbapi.com/?t=matrix&apikey=a10bdbc
 @SpringBootApplication
 public class CatFlixApplication implements CommandLineRunner {
 	public static void main(String[] args){
@@ -12,7 +13,8 @@ public class CatFlixApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-		System.out.println("Hello, World!");
-		System.out.println("Primeiro projeto Spring sem Web");
+		var consumoApi = new ConsumoApi();
+		var json = consumoApi.obterDados("https://www.omdbapi.com/?t=friends&Season=1&apikey=a10bdbc");
+		System.out.println(json);
 	}
 }
