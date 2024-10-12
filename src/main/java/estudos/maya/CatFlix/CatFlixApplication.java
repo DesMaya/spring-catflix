9a707d2bcf06f5d81d553d78f731e8f74e158b20
Maya <mayaflorentinodasilva@gmail.com>
2024-10-12 18:33:00 -0300

    Utilizando o conversor para mapear e converter os dados da api

--- src/main/java/estudos/maya/CatFlix/CatFlixApplication.java
+++ src/main/java/estudos/maya/CatFlix/CatFlixApplication.java
@@ -1,6 +1,8 @@
 package estudos.maya.CatFlix;
 
 import estudos.maya.CatFlix.service.ConsumoApi;
+import estudos.maya.model.DadosSerie;
+import estudos.maya.service.ConverteDados;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -14,7 +16,10 @@ public class CatFlixApplication implements CommandLineRunner {
 	@Override
 	public void run(String... args) throws Exception {
 		var consumoApi = new ConsumoApi();
-		var json = consumoApi.obterDados("https://www.omdbapi.com/?t=friends&Season=1&apikey=a10bdbc");
-		System.out.println(json);
+		var conversor = new ConverteDados();
+
+		var json = consumoApi.obterDados("https://www.omdbapi.com/?t=friends&apikey=a10bdbc");
+		var dadosSerie = conversor.obterDados(json, DadosSerie.class);
+		System.out.println(dadosSerie);
 	}
 }
