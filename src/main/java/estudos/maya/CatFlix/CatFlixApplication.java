1d4c96c0d530d26565608a915f85a4aae638b530
Maya <mayaflorentinodasilva@gmail.com>
2024-10-12 16:26:03 -0300

    implementando a CommandLineRunner para utilizar no projeto

---
+++ src/main/java/estudos/maya/CatFlix/CatFlixApplication.java
@@ -0,0 +1,18 @@
+package estudos.maya.CatFlix;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatFlixApplication implements CommandLineRunner {
+	public static void main(String[] args){
+		SpringApplication.run(CatFlixApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		System.out.println("Hello, World!");
+		System.out.println("Primeiro projeto Spring sem Web");
+	}
+}
---
+++ src/test/java/estudos/maya/CatFlix/CatFlixApplicationTests.java
@@ -0,0 +1,13 @@
+package estudos.maya.CatFlix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CatFlixApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
