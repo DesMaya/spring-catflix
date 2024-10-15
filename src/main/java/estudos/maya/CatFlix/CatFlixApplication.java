7d44634f87549947de9c8f97611d67b700606182
Maya <mayaflorentinodasilva@gmail.com>
2024-10-15 15:14:56 -0300

    removendo comentários desnecessários

--- src/main/java/estudos/maya/CatFlix/CatFlixApplication.java
+++ src/main/java/estudos/maya/CatFlix/CatFlixApplication.java
@@ -13,26 +13,6 @@ public class CatFlixApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-		/*
-		var consumoApi = new ConsumoApi();
-		var conversor = new ConverteDados();
-
-		var json = consumoApi.obterDados("https://www.omdbapi.com/?t=friends&apikey=a10bdbc");
-		var dadosSerie = conversor.obterDados(json, DadosSerie.class);
-		System.out.println(dadosSerie);
-
-		json = consumoApi.obterDados("https://www.omdbapi.com/?t=friends&Season=4&Episode=8&apikey=a10bdbc");
-		var dadosEpisodio = conversor.obterDados(json, DadosEpisodio.class);
-		System.out.println(dadosEpisodio);
-
-		List<DadosTemporada> temporadas = new ArrayList<>();
-		for (int i = 1; i <= dadosSerie.totalTemporadas(); i++){
-			json = consumoApi.obterDados("https://www.omdbapi.com/?t=friends&Season=" + i + "&apikey=a10bdbc");
-			temporadas.add(conversor.obterDados(json, DadosTemporada.class));
-		}
-
-		temporadas.forEach(System.out::println);
-		*/
 		Principal principal = new Principal();
 		principal.exibeMenu();
 	}
