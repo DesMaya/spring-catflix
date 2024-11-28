411958d3ba6e0e375a3d357a41ed819196235efd
Maya <mayaflorentinodasilva@gmail.com>
2024-11-28 19:22:32 -0300

    Criamos o construtor da classe atribuindo cada valor a seu respectivo atributo utilizando as funçoes de confersão quando nescessárias

--- src/main/java/estudos/maya/CatFlix/model/Serie.java
+++ src/main/java/estudos/maya/CatFlix/model/Serie.java
@@ -2,6 +2,8 @@ package estudos.maya.CatFlix.model;
 
 import com.fasterxml.jackson.annotation.JsonAlias;
 
+import java.util.OptionalDouble;
+
 public class Serie {
     private String titulo;
     private Categoria genero;
@@ -10,4 +12,14 @@ public class Serie {
     private String atores;
     private String sinopse;
     private String poster;
+
+    public Serie (DadosSerie dadosSerie) {
+        this.titulo = dadosSerie.titulo();
+        this.totalTemporadas = dadosSerie.totalTemporadas();
+        this.avaliacao = OptionalDouble.of(Double.valueOf(dadosSerie.avaliacao())).orElse(0.0);
+        this.genero = Categoria.valueOf(dadosSerie.genero().split(",")[0].trim());
+        this.atores = dadosSerie.atores();
+        this.sinopse = dadosSerie.sinopse();
+        this.poster = dadosSerie.poster();
+    }
 }
