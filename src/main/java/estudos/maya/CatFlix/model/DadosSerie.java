57383cdeed5ef5773ac527b05c804bdc9e18a63a
Maya <mayaflorentinodasilva@gmail.com>
2024-11-28 18:46:09 -0300

    Adicionando atores, sinopse, poster e genero ao record

--- src/main/java/estudos/maya/CatFlix/model/DadosSerie.java
+++ src/main/java/estudos/maya/CatFlix/model/DadosSerie.java
@@ -6,7 +6,11 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 @JsonIgnoreProperties(ignoreUnknown = true)
 public record DadosSerie(
         @JsonAlias("Title") String titulo,
+        @JsonAlias("Genre") String genero,
         @JsonAlias("totalSeasons") Integer totalTemporadas,
-        @JsonAlias("imdbRating") String avaliacao
+        @JsonAlias("imdbRating") String avaliacao,
+        @JsonAlias("Actors") String atores,
+        @JsonAlias("Plot") String sinopse,
+        @JsonAlias("Poster") String poster
 ) {
 }
