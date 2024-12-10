39d27db4fd0f026f6d143a177d1e1a22f47e44bb
Maya <mayaflorentinodasilva@gmail.com>
2024-12-10 18:34:56 -0300

    Criando o repositorio para ser uma variavel de ambiente sinalizando ela para o Spring e repassando ela como parâmetro na minha classe principal

--- src/main/java/estudos/maya/CatFlix/CatFlixApplication.java
+++ src/main/java/estudos/maya/CatFlix/CatFlixApplication.java
@@ -1,19 +1,25 @@
 package estudos.maya.CatFlix;
 
 import estudos.maya.CatFlix.principal.Principal;
+import estudos.maya.CatFlix.repository.SerieRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class CatFlixApplication implements CommandLineRunner {
+
+	@Autowired
+	private SerieRepository repositorio;
+
 	public static void main(String[] args){
 		SpringApplication.run(CatFlixApplication.class, args);
 	}
 
 	@Override
 	public void run(String... args) throws Exception {
-		Principal principal = new Principal();
+		Principal principal = new Principal(repositorio);
 		principal.exibeMenu();
 	}
 }
