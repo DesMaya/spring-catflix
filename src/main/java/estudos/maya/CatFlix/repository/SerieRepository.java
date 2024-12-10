e3f64ab154aaf459d7dfeb02117327071d8b08ff
Maya <mayaflorentinodasilva@gmail.com>
2024-12-10 18:36:53 -0300

    Criada uma interface para repositorio utilizando a JpaRepository para lidar com nossas séries no banco de dados

---
+++ src/main/java/estudos/maya/CatFlix/repository/SerieRepository.java
@@ -0,0 +1,8 @@
+package estudos.maya.CatFlix.repository;
+
+import estudos.maya.CatFlix.model.Serie;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SerieRepository extends JpaRepository<Serie, Long> {
+
+}
