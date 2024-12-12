263017cf41082a87fececc0ea2030c64ab72c17e
Maya <mayaflorentinodasilva@gmail.com>
2024-12-12 19:03:16 -0300

    Implementada função buscar série por titulo que utiliza da jpa para buscar séries no banco a partir de trechos do nome, também foi aplicada essa função na buscar eisodios

--- src/main/java/estudos/maya/CatFlix/principal/Principal.java
+++ src/main/java/estudos/maya/CatFlix/principal/Principal.java
@@ -36,6 +36,7 @@ public class Principal {
                     1 - Buscar séries
                     2 - Buscar episódios
                     3 - Listar séries buscadas
+                    4 - Buscar serie por título
                     
                     0 - Sair                                 
                     """;
@@ -53,6 +54,9 @@ public class Principal {
                     break;
                 case 3:
                     listarSeriesBuscadas();
+                    break;
+                case 4:
+                    buscarSeriePorTitulo();
                     break;
                 case 0:
                     System.out.println("Saindo...");
@@ -84,9 +88,12 @@ public class Principal {
         System.out.println("Escolha uma série pelo nome: ");
         var nomeSerie = sc.nextLine();
 
+        Optional<Serie> serie = repositorio.findByTituloContainingIgnoreCase(nomeSerie);
+        /*
         Optional<Serie> serie = series.stream()
                 .filter(s -> s.getTitulo().toLowerCase().contains(nomeSerie.toLowerCase()))
                 .findFirst();
+         */
 
         if (serie.isPresent()) {
             var serieEncontrada = serie.get();
@@ -121,6 +128,19 @@ public class Principal {
         series.stream()
                 .sorted(Comparator.comparing(Serie::getGenero))
                 .forEach(System.out::println);
+    }
+
+    private void buscarSeriePorTitulo() {
+        System.out.println("Escolha uma série pelo nome: ");
+        var nomeSerie = sc.nextLine();
+
+        Optional<Serie> serieBuscada = repositorio.findByTituloContainingIgnoreCase(nomeSerie);
+
+        if(serieBuscada.isPresent()) {
+            System.out.println("Dados da série: "  + serieBuscada.get());
+        } else {
+            System.out.println("Série não encontrada!");
+        }
     }
 
 }
