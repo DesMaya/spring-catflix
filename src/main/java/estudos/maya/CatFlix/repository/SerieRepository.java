0c0075f808729e6f563e1c614c24bf15642ac18b
Maya <mayaflorentinodasilva@gmail.com>
2024-12-12 19:34:21 -0300

    Criada a busca com base no nome do ator e uma avaliação minima

--- src/main/java/estudos/maya/CatFlix/repository/SerieRepository.java
+++ src/main/java/estudos/maya/CatFlix/repository/SerieRepository.java
@@ -3,8 +3,10 @@ package estudos.maya.CatFlix.repository;
 import estudos.maya.CatFlix.model.Serie;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface SerieRepository extends JpaRepository<Serie, Long> {
     Optional<Serie> findByTituloContainingIgnoreCase(String nomeSerie);
+    List<Serie> findByAtoresContainingIgnoreCaseAndAvaliacaoGreaterThanEqual(String nomeAtor, double avaliacao);
 }
