51331c6d565f6a68e848f847beed55b5f7d6ac54
Maya <mayaflorentinodasilva@gmail.com>
2024-12-18 17:32:13 -0300

    Ajustando a aplicação para web

--- src/main/java/estudos/maya/CatFlix/CatFlixApplication.java
+++ src/main/java/estudos/maya/CatFlix/CatFlixApplication.java
@@ -1,25 +1,12 @@
 package estudos.maya.CatFlix;
 
-import estudos.maya.CatFlix.principal.Principal;
-import estudos.maya.CatFlix.repository.SerieRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CatFlixApplication implements CommandLineRunner {
-
-	@Autowired
-	private SerieRepository repositorio;
+public class CatFlixApplication {
 
 	public static void main(String[] args){
 		SpringApplication.run(CatFlixApplication.class, args);
-	}
-
-	@Override
-	public void run(String... args) throws Exception {
-		Principal principal = new Principal(repositorio);
-		principal.exibeMenu();
 	}
 }
