c7cc52109d91f51478804f7e0633045cfe5c1d9e
Maya <mayaflorentinodasilva@gmail.com>
2024-12-18 18:05:53 -0300

    Refatorando o controller para entregar uma lista de séries

--- src/main/java/estudos/maya/CatFlix/controller/SerieController.java
+++ src/main/java/estudos/maya/CatFlix/controller/SerieController.java
@@ -1,14 +1,43 @@
 package estudos.maya.CatFlix.controller;
 
+import estudos.maya.CatFlix.dto.SerieDTO;
+import estudos.maya.CatFlix.repository.SerieRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 public class SerieController {
 
+    @Autowired
+    private SerieRepository repository;
+
     @GetMapping("/series")
-    public String obterSeries(){
-        return "Aqui vão ser listadas as séries";
+    public List<SerieDTO> obterSeries(){
+        return repository.findAll().stream()
+                .map(s -> new SerieDTO(
+                        s.getId(),
+                        s.getTitulo(),
+                        s.getGenero(),
+                        s.getTotalTemporadas(),
+                        s.getAvaliacao(),
+                        s.getAtores(),
+                        s.getSinopse(),
+                        s.getPoster()))
+                .collect(Collectors.toList());
     }
 
 }
+/*
+    Long id,
+     String titulo,
+     Categoria genero,
+     Integer totalTemporadas,
+     Double avaliacao,
+     String atores,
+     String sinopse,
+     String poster
+ */
