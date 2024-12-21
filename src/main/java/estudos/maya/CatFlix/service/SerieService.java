d7888d597bb6907be90077e99d7fa87381b52517
Maya <mayaflorentinodasilva@gmail.com>
2024-12-21 19:01:32 -0300

    Refatorando a a classe service para melhor usabilidade e acrescentando o map para o top5

--- src/main/java/estudos/maya/CatFlix/service/SerieService.java
+++ src/main/java/estudos/maya/CatFlix/service/SerieService.java
@@ -1,6 +1,7 @@
 package estudos.maya.CatFlix.service;
 
 import estudos.maya.CatFlix.dto.SerieDTO;
+import estudos.maya.CatFlix.model.Serie;
 import estudos.maya.CatFlix.repository.SerieRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -15,7 +16,15 @@ public class SerieService {
     private SerieRepository repository;
 
     public List<SerieDTO> obterTodasAsSeries() {
-        return repository.findAll().stream()
+        return converteDados(repository.findAll());
+    }
+
+    public List<SerieDTO> obterTop5Series() {
+        return converteDados(repository.findTop5ByOrderByAvaliacaoDesc());
+    }
+
+    private List<SerieDTO> converteDados (List<Serie> series){
+        return series.stream()
                 .map(s -> new SerieDTO(
                         s.getId(),
                         s.getTitulo(),
